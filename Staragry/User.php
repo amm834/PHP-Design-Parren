70f21268d507ca8f499c5f2dcbc39aadbc446956
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2020-11-27 21:23:07 +0630

    Stragery Pattern 1.1

--- Staragry/User.php
+++ Staragry/User.php
@@ -2,10 +2,23 @@
 namespace App\Staragry;
 class User
 {
-  
-  public function __construct()
-  {
-    $obj = new SMSNotify;
+
+  public function __construct($type) {
+    $obj = null;
+    switch ($type) {
+      case $obj = 'SMS':
+        $obj = new SMSNotify();
+        break;
+      case $obj = 'Email':
+        $obj = new EmailNotify();
+        break;
+      case $obj = 'Phone':
+        $obj = new PhoneNotify();
+        break;
+      default:
+        echo 'Only Email,SMS and Phone notify!';
+        break;
+    }
     $obj->send();
   }
 }
--- index.php
+++ index.php
@@ -2,4 +2,8 @@
 
 require_once 'vendor/autoload.php';
 use App\Staragry\User;
-new User;
+new User('SMS');
+echo '<br>';
+new User('Email');
+echo '<br>';
+new User('Phone');
